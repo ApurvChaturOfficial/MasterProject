858a2d3045c7be2f14e453a5c92f75415bb0c63c
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-05-02 19:20:14 +0530

    Environment Variables Fixed

--- Backend/backend/src/connection/aDatabase.js
+++ Backend/backend/src/connection/aDatabase.js
@@ -9,12 +9,12 @@ const database = () => {
 
     var DB_URL
         
-    process.env.ACTIVE_APP === 'SampleAuthenticationApp' ? DB_URL = process.env.DB_URL1 :
-    process.env.ACTIVE_APP === 'SampleAdministrationApp' ? DB_URL = process.env.DB_URL2 :
-    process.env.ACTIVE_APP === 'PersonalPortfolioApp' ? DB_URL = process.env.DB_URL3 :
-    process.env.ACTIVE_APP === 'NehaPortfolioApp' ? DB_URL = process.env.DB_URL4 :
-    process.env.ACTIVE_APP === 'AnnushreePortfolioApp' ? DB_URL = process.env.DB_URL5 :
-    DB_URL =process.env.DB_URL1
+    process.env.ACTIVE_APP === 'SampleAuthenticationApp' ? DB_URL = DB_URL1 :
+    process.env.ACTIVE_APP === 'SampleAdministrationApp' ? DB_URL = DB_URL2 :
+    process.env.ACTIVE_APP === 'PersonalPortfolioApp' ? DB_URL = DB_URL3 :
+    process.env.ACTIVE_APP === 'NehaPortfolioApp' ? DB_URL = DB_URL4 :
+    process.env.ACTIVE_APP === 'AnnushreePortfolioApp' ? DB_URL = DB_URL5 :
+    DB_URL =DB_URL1
 
     mongoose.set('strictQuery', true);
     mongoose
