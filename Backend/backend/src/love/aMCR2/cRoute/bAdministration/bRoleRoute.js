b8c577b024061dc1984d7e182c8ecf1e478a2f36
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-05-03 16:59:29 +0530

    New Deploy

--- Backend/backend/src/love/aMCR2/cRoute/bAdministration/bRoleRoute.js
+++ Backend/backend/src/love/aMCR2/cRoute/bAdministration/bRoleRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.route("/list").get(roleController().list);
-router.route("/create").post(authenticateUser(UserModel), roleController().create);
+router.route("/create").post(roleController().create);
 router.route("/retrieve/:id").get(authenticateUser(UserModel), roleController().retrieve);
 router.route("/update/:id").put(authenticateUser(UserModel), roleController().update);
 router.route("/delete/:id").delete(authenticateUser(UserModel), roleController().delete);
