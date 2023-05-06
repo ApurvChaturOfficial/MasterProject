1d61e38f0668343d88548208c468f4e3ccf6efe0
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-05-07 01:11:30 +0530

    Index Fixed

--- Frontend3/frontend3/src/index.js
+++ Frontend3/frontend3/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index2.css'
 import { Provider } from 'react-redux'
 import Store from './love/bRedux/Store';
+process.env.REACT_APP_ACTIVE_APP === "PersonalPortfolioApp" ? import('./index1.css') :
+process.env.REACT_APP_ACTIVE_APP === "NehaPortfolioApp" ? import('./index2.css') :
+process.env.REACT_APP_ACTIVE_APP === "AnushreePortfolioApp" ? import('./index3.css') : import('./index1.css')
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
