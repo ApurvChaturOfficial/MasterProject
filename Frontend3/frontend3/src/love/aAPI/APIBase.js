6663eb6737ba1a0a19985c455fbe8dbe661454bb
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-05-13 20:31:15 +0530

    For Live

--- Frontend3/frontend3/src/love/aAPI/APIBase.js
+++ Frontend3/frontend3/src/love/aAPI/APIBase.js
@@ -6,13 +6,13 @@ const APIBase = (props) => {
     const configuration = {
         method: props.method,
         url: props.url,
-        baseURL: "http://localhost:4000",
-        // baseURL: (
-        //     process.env.REACT_APP_ACTIVE_APP === "PersonalPortfolioApp" ? "https://ac-personal-portfolio-app.onrender.com" : 
-        //     process.env.REACT_APP_ACTIVE_APP === "NehaPortfolioApp" ? "https://ac-neha-portfolio-app.onrender.com" :
-        //     process.env.REACT_APP_ACTIVE_APP === "AnushreePortfolioApp" ? "https://ac-anushree-portfolio-app.onrender.com" :
-        //     "adasd"
-        // ),
+        // baseURL: "http://localhost:4000",
+        baseURL: (
+            process.env.REACT_APP_ACTIVE_APP === "PersonalPortfolioApp" ? "https://ac-personal-portfolio-app.onrender.com" : 
+            process.env.REACT_APP_ACTIVE_APP === "NehaPortfolioApp" ? "https://ac-neha-portfolio-app.onrender.com" :
+            process.env.REACT_APP_ACTIVE_APP === "AnushreePortfolioApp" ? "https://ac-anushree-portfolio-app.onrender.com" :
+            "adasd"
+        ),
         headers: { 
             'Content-Type': 'application/json',
         },
